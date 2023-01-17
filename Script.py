9aaa29f858a3d9ec4785eb612c2facfe5492bbdf
srivarshi12 <122310000+srivarshi12@users.noreply.github.com>
2023-01-17 06:29:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -2,23 +2,23 @@ class script(object):
     START_TXT = """Hello, I am Auto Filter bot 🚀 With Same more Features ✨"""
     HELP_TXT = """MORE INFORMATION IS HERE✨."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/umlinks>Umlinks</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Mvrequ>MVRequest</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
-    SOURCE_TXT = """<b>NOTE:</b>
+    # SOURCE_TXT = """<b>NOTE:</b>
 - Auto Filter bot is a open source project. 
 - Source - https://github.com/selfie-bd/autofilterv5  
 
-<b>DEVS:</b>
+# <b>DEVS:</b>
 - <a href=https://t.me/umlinks>@UmLinks</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and autofilterv5 will respond whenever a keyword is found the message
 
-<b>NOTE:</b>
+ <b>NOTE:</b>
 1. eva maria should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
@@ -38,7 +38,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/umlinks)</code>
+<code>[Button Text](buttonurl:https://t.me/mvrequ)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
